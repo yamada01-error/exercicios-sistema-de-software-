b102b2a3ed8aef1d7e885022acca0afdc471f67d
Yamada <igoryamada57@gmail.com>
2026-05-28 22:25:58 -0300

    Implement basic login functionality

--- fácil/🟢-sistema-login.js
+++ fácil/🟢-sistema-login.js
@@ -1,1 +1,8 @@
-///
+let usuario = "admin";
+let senha = "1234";
+
+if (usuario === "admin" && senha === "1234") {
+    console.log("Login realizado com sucesso");
+} else {
+    console.log("Usuário ou senha incorretos");
+}
