80bb25a2cdfb83357b8c6fd52f6cb4d39fd45e46
Yamada <igoryamada57@gmail.com>
2026-05-28 22:30:15 -0300

    Add grading system in 🟢-sistema-notas.js

---
+++ fácil/🟢-sistema-notas.js
@@ -0,0 +1,12 @@
+let portugues = 8;
+let matematica = 6;
+
+let media = (portugues + matematica) / 2;
+
+console.log("Média:", media);
+
+if (media >= 7) {
+    console.log("Aluno aprovado");
+} else {
+    console.log("Aluno reprovado");
+}
