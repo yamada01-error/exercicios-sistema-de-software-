4680ca4273da17c1886f9bc989646ddf26e141fd
Yamada <igoryamada57@gmail.com>
2026-05-28 22:36:29 -0300

    Add basic library management functionality

---
+++ médio/🟡-sistema-biblioteca.js
@@ -0,0 +1,12 @@
+let biblioteca = [];
+
+biblioteca.push("Dom Casmurro");
+biblioteca.push("Harry Potter");
+biblioteca.push("O Hobbit");
+
+console.log("Livros disponíveis:", biblioteca);
+
+let removido = biblioteca.pop();
+
+console.log("Livro removido:", removido);
+console.log("Biblioteca atual:", biblioteca);
