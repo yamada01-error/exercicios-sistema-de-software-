6fe9bbbeba033e77054792ff5f85560cd00abde5
Yamada <igoryamada57@gmail.com>
2026-05-28 22:37:47 -0300

    Add parking system functionality in JavaScript

---
+++ médio/🟡-sistema-estacionamento.js
@@ -0,0 +1,12 @@
+let estacionamento = [];
+
+estacionamento.push("Fusca");
+estacionamento.push("Civic");
+estacionamento.push("Corolla");
+
+console.log("Carros estacionados:", estacionamento);
+
+let saiu = estacionamento.shift();
+
+console.log("Carro que saiu:", saiu);
+console.log("Vagas atuais:", estacionamento);
